e900add725ae686026670e00a314e7669fdfda8d
homeira kalim <homeirakalim@gmail.com>
2025-02-11 12:45:42 -0500

    draft with js file deliverables

---
+++ index.js
@@ -0,0 +1,34 @@
+
+//function clearPage{ 
+//which will clear everything except for the h1 heding on the page}
+//code goes here:
+
+//addEvenListener # 1?
+//listens for click on 'Browse Movies"
+//calls the clearPage function()
+//handles the event by displaying all movies
+//code goes here:
+
+
+//add.EventListener #2
+//listens for a 'form' submit
+//does a strict equality comparison between the movie title entered and the movies in th db.json file (I don't think it should be case sensitive)
+//regardless of whether it finds a match, 
+//calls the clearPage function()
+//if it finds a match, it returns the movie listing page 
+//if it doesn't find a match "Sorry, not in our data base."
+//stretch goal: if it's not in the data base, we allow the user to submit the title online, for us to add
+//code goes here:
+
+//add.EventListener #3
+//listens for a 'mousover' over the id="movie-details" box
+//calls the clearPage function()
+//will grab the <h2> and <p> tags in the "movie-details" div and console log the information on the page
+//will contain an addEventListener for a click on the thumbs or thumbs button
+//code goes here:
+
+
+//addEventListener #4?
+//submit 'form' of filters
+
+
